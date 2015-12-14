a57988871a3ba64db425181a1c1271719d0169cb
kingrake <kingrake@163.com>
2015-12-14 13:41:17 +0800

    Initial Commit

---
+++ MyiOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyiOne
+//
+//  Created by Lichen King on 15/12/14.
+//  Copyright © 2015年 Lichen King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyiOne/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyiOne
+//
+//  Created by Lichen King on 15/12/14.
+//  Copyright © 2015年 Lichen King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
